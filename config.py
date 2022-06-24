473cebeb5e976d99b2f58825a509d5a0aac0c8c0
Rodrigo <rcbm539@gmail.com>
2022-06-23 23:31:52 -0300

    Primeiro commit

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ DB='contas_flutter'
 
 #SQLALCHEMY_DATABASE_URI=f'mysql://{USERNAME}:{PASSWORD}@{SERVER}/{DB}'
 
-SQLALCHEMY_DATABASE_URI='postgresql://ayvqfzsdizyhun:d681c8ddfa469b6174bfbc8202e272cf223414e1cb6a71faeae17bfb47e029a4@ec2-52-22-136-117.compute-1.amazonaws.com:5432/djgj5esg4j6d8'
+SQLALCHEMY_DATABASE_URI='postgresql://aiatgoudcuwaak:c99af795f505686cf7c7d33703799c6080bba2bbd4220f82315557800041e44c@ec2-44-197-128-108.compute-1.amazonaws.com:5432/d1l47d9mudru00'
 
 SQLALCHEMY_TRACK_MODIFICATIONS=True
 
